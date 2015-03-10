65b95830d2c060707aad4e571cfb3ad788a9c027
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-03-10 16:37:07 -0700

    changing attributes all over

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,6 +15,8 @@ service "httpd" do
   action [:enable, :start]
 end
 
+node.default["apache"]["indexfile"] = "index2.html"
+
 cookbook_file "/var/www/html/index.html" do
   source node["apache"]["indexfile"]
   mode '0644'
