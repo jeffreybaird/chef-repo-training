f377c8fb3c0b78b2b19d875850e5a86eb619c289
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-03-11 11:29:30 -0700

    adding some magic

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,9 +15,42 @@ service "httpd" do
   action [:enable, :start]
 end
 
-node.default["apache"]["indexfile"] = "index2.html"
-
-cookbook_file "/var/www/html/index.html" do
-  source node["apache"]["indexfile"]
-  mode '0644'
+execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/elcome.conf.disabled" do
+  only_if {File.exist?('/etc/httpd/conf.d/welcome.conf'}
+  notifies :restart, 'service[httpd]'
 end
+
+node["apache"]["sites"].each do |site_name,site_data|
+
+  document_root = "/srv/apache/#{site_name}"
+
+
+  template "/etc/httpd /conf.d/#{site_name}.conf.d" do
+    source 'custom.erb'
+    mode '0644'
+    variables(
+        document_root: document_root,
+        port: site_data["port"]
+      )
+    notifies :restart, 'service[httpd]'
+  end
+
+  directory "#{document_root}" do
+    mode '0755'
+    recursive :true
+    action :create
+  end
+
+  template "#{document_root}/index.html" do
+    source 'index.html.erb'
+    mode '0644'
+    variables(
+      site_name: site_name,
+      port: site_data["port"]
+      )
+  end
+
+
+end
+
+
